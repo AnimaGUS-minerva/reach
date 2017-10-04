bf457ae65b95c5ef6f0bce98837d8bce5d091e02
Michael Richardson <mcr@sandelman.ca>
2017-10-04 13:26:51 -0400

    generate a voucher request from information the IDevID

--- spec/lib/pledge_keys_spec.rb
+++ spec/lib/pledge_keys_spec.rb
@@ -2,17 +2,41 @@ require 'pledge_keys'
 
 RSpec.describe PledgeKeys do
 
-  describe "IDevID public key" do
+  describe "IDevID certificate" do
     it "should be a public key" do
       b = PledgeKeys.instance.idevid_pubkey
       expect(b).to be_kind_of(OpenSSL::X509::Certificate)
     end
   end
 
+  def cmp_pkcs_file(smime, base)
+    ofile = File.join("tmp", base + ".pkcs")
+    otfile = File.join("tmp", base+ ".txt")
+
+    File.open(ofile, "w") do |f|     f.puts smime      end
+
+    system("bin/pkcs2json #{ofile} #{otfile}")
+    cmd = "diff #{otfile} spec/files/#{base}.txt"
+    puts cmd
+    system(cmd)
+  end
+
   describe "IDevID private key" do
     it "should be a kind of private key" do
       b = PledgeKeys.instance.idevid_privkey
       expect(b).to be_kind_of(OpenSSL::PKey::PKey)
+    end
+
+    it "should pkcs sign a voucher request" do
+      vr = Chariwt::VoucherRequest.new
+      vr.nonce = "Dss99sBr3pNMOACe-LYY7w"
+      vr.assertion = :proximity
+      vr.signing_cert = PledgeKeys.instance.idevid_pubkey
+      vr.serialNumber = vr.eui64_from_cert
+      #vr.proximityRegistrarCert = get_cert_from_tls
+      smime = vr.pkcs_sign(PledgeKeys.instance.idevid_privkey)
+
+      cmp_pkcs_file(smime, "pledge_request01")
     end
   end
 
@@ -22,6 +46,5 @@ RSpec.describe PledgeKeys do
       expect(b).to be_kind_of(OpenSSL::X509::Certificate)
     end
   end
-
 
 end
