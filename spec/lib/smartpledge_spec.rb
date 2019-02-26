b0bbf847c554ce44710273e856d9a9aeccf8bf2a
Michael Richardson <mcr@sandelman.ca>
2019-02-26 18:04:54 -0500

    validate that self-signed certificate reuses private key

--- spec/lib/smartpledge_spec.rb
+++ spec/lib/smartpledge_spec.rb
@@ -7,7 +7,7 @@ require 'rails_helper'
 RSpec.describe SmartPledge do
 
   describe "creating self-signed certificate" do
-    def tmp_pledge_dir
+    def mk_pledge_dir
       newdir = Rails.root.join("tmp").join("pledge1")
       FileUtils.remove_entry_secure(newdir) if Dir.exists?(newdir)
       FileUtils.mkdir_p(newdir)
@@ -16,10 +16,28 @@ RSpec.describe SmartPledge do
 
     it "should create a new cert if needed" do
       sp = SmartPledge.new
-      pdir = tmp_pledge_dir
+      pdir = mk_pledge_dir
 
       sp.generate_selfidevid(pdir)
       expect(File.exists?(PledgeKeys.instance.pub_file))
+    end
+
+    def tmp_pledge_dir(srcdir)
+      ndir = mk_pledge_dir
+      system("cp -r #{srcdir}/. #{ndir}/.")
+    end
+
+    it "should reuse the private key if already created" do
+      sp = SmartPledge.new
+      pdir = tmp_pledge_dir("spec/files/product/SmartPledge-1502449999")
+
+      expect(File.exists?(PledgeKeys.instance.priv_file))
+      orig = OpenSSL::Digest.digest("SHA256", IO::read(PledgeKeys.instance.priv_file))
+      sp.generate_selfidevid(pdir)
+      expect(File.exists?(PledgeKeys.instance.pub_file))
+
+      newd = OpenSSL::Digest.digest("SHA256", IO::read(PledgeKeys.instance.priv_file))
+      expect(newd).to eq(orig)
     end
   end
 
