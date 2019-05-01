a2dc0926d693ea65de05d0e344f8de492c13b7de
Michael Richardson <mcr@sandelman.ca>
2019-05-01 10:01:24 -0400

    enroll with MASA, produce the ldevice.crt for later testing

--- sk/step01.sh
+++ sk/step01.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bundle exec rake reach:r0_dpp_manu_enroll DPPFILE=spec/files/dpp1.txt PRODUCTID=spec/files/product/Smarkaklink-1502449999
+bundle exec rake reach:sk0_dpp_manu_enroll DPPFILE=spec/files/dpp1.txt PRODUCTID=spec/files/product/Smarkaklink-1502449999
 
