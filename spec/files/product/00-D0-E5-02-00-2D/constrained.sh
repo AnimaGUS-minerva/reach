917ddf6cbbb1f879e5d41157b3a8efd039b76689
Michael Richardson <mcr@sandelman.ca>
2019-05-15 16:47:03 -0400

    added scripts to generate vouchers for examples

---
+++ spec/files/product/00-D0-E5-02-00-2D/constrained.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+exec rake reach:send_constrained_request PRODUCTID=00-D0-E5-E0-00-0F  JRC=coaps://fountain-test.sandelman.ca/
+
---
+++ spec/files/product/00-D0-E5-02-00-2D/voucher.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd $(dirname $0)/../../../..
+rake reach:enroll_http_pledge PRODUCTID=spec/files/product/00-D0-E5-02-00-2D JRC=https://fountain-test.sandelman.ca:8443/
+
+
