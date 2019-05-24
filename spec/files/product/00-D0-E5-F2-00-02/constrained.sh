8308904ba056d19200a0ff988d0cc98f0a4ed240
Michael Richardson <mcr@sandelman.ca>
2019-05-24 14:54:42 -0400

    generated and capture examples of constrained enrollment

--- spec/files/product/00-D0-E5-F2-00-02/constrained.sh
+++ spec/files/product/00-D0-E5-F2-00-02/constrained.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd $(dirname $0)/../../../..
-rake reach:send_constrained_request PRODUCTID=spec/files/product/00-D0-F2-02-00-02 JRC=coaps://fountain-test.sandelman.ca/
+rake reach:send_constrained_request PRODUCTID=spec/files/product/00-D0-E5-F2-00-02 JRC=coaps://fountain-test.sandelman.ca/
 
 
 
