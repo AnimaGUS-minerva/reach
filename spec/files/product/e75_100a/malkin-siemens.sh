2a0b152aadc17819aaad3f0de8f0fc3a5de6410a
Michael Richardson <mcr@sandelman.ca>
2019-07-02 12:44:48 -0400

    added test files for interop testing with Siemens

---
+++ spec/files/product/e75_100a/malkin-siemens.sh
@@ -0,0 +1,1 @@
+rake reach:enroll_http_pledge PRODUCTID=spec/files/product/e75_100a JRC=https://malkin.sandelman.ca:8443/
---
+++ spec/files/product/e75_100a/siemens-siemens.sh
@@ -0,0 +1,1 @@
+rake reach:enroll_http_pledge PRODUCTID=spec/files/product/e75_100a JRC=https://registrar-test.siemens-bt.net:8443/
