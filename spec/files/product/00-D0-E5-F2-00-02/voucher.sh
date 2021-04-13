443e26d1dc6b84f0caf069ca74ca0928302e6ca9
Michael Richardson <mcr@sandelman.ca>
2021-04-13 17:25:03 -0400

    updated voucher, idevid and ldevid

--- spec/files/product/00-D0-E5-F2-00-02/voucher.sh
+++ spec/files/product/00-D0-E5-F2-00-02/voucher.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-cd $(dirname $0)/../../../..
-rake reach:enroll_http_pledge PRODUCTID=spec/files/product/00-D0-E5-F2-00-02 JRC=https://fountain-test.sandelman.ca:8443/
+(cd $(dirname $0)/../../../..
+ rake reach:enroll_http_pledge PRODUCTID=spec/files/product/00-D0-E5-F2-00-02 JRC=https://fountain-test.sandelman.ca:8443/
+)
 
+mv ../../../../tmp/csr* .
+mv ../../../../tmp/voucher_00-D0-E5-F2-00-02.pkcs .
+mv ../../../../tmp/vr_00-D0-E5-F2-00-02.pkcs .
 
