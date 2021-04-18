e8b475686720d23cfcd0b835af3ae83a68f129b8
Michael Richardson <mcr@sandelman.ca>
2021-04-18 19:59:35 -0400

    update with correct matching certificate!

--- spec/files/product/00-D0-E5-F2-00-11/voucher.sh
+++ spec/files/product/00-D0-E5-F2-00-11/voucher.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+(
 cd $(dirname $0)/../../../..
-rake reach:enroll_http_pledge PRODUCTID=spec/files/product/00-D0-E5-F2-00-10 JRC=https://fountain-test.sandelman.ca:8443/
+rake reach:enroll_http_pledge PRODUCTID=spec/files/product/00-D0-E5-F2-00-11 JRC=https://fountain-test.sandelman.ca:8443/
+)
+
+mv ../../../../tmp/csr* .
+mv ../../../../tmp/voucher_00-D0-E5-F2-00-11.pkcs .
+mv ../../../../tmp/vr_00-D0-E5-F2-00-11.pkcs .
+mv ../../../../tmp/certificate.der f20011.crt
+
+
 
 
