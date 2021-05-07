b6c693d92473a07dfad95912b7972d227dd2ec80
Michael Richardson <mcr@sandelman.ca>
2021-05-07 15:23:11 -0400

    added certificate for ovid

---
+++ spec/files/product/00-D0-E5-01-00-16/voucher.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+prod=$(basename $(/bin/pwd))
+(
+    cd $(dirname $0)/../../../..
+rake reach:enroll_http_pledge PRODUCTID=spec/files/product/${prod} JRC=https://fountain-test.sandelman.ca:8443/
+)
+
+mv ../../../../tmp/csr* .
+mv ../../../../tmp/voucher_${prod}.pkcs .
+mv ../../../../tmp/vr_${prod}.pkcs .
+mv ../../../../tmp/certificate.der ldevid.crt
+
+
+
+
